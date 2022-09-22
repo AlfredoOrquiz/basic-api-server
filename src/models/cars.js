e5e96c41120a74ce160e6d7ba81da3600d4a73fe
Alfredo <fredy_rios0123@outlook.com>
2022-09-22 05:37:03 +0000

    Made changes to dev

--- src/models/cars.js
+++ src/models/cars.js
@@ -1,2 +1,13 @@
 'use strict';
 
+const carsSchema = (sequelize, DataTypes) => sequelize.define(
+  'Cars',
+  {
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  }
+);
+
+module.exports = carsSchema;
